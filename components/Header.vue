4837e3999ba27baf5ade37c4f0afb519b1fb2653
refrains <refrains@blair.nz>
2023-07-29 20:03:50 +1200

    Remove Tailwind mark

--- components/Header.vue
+++ components/Header.vue
@@ -47,15 +47,10 @@
         <div class="flex items-center justify-between">
           <nuxt-link
             to="/"
-            class="-m-1.5 p-1.5"
+            class="-m-1.5 p-1.5 font-bold"
             @click="mobileMenuOpen = false"
           >
-            <span class="sr-only">Your Company</span>
-            <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
-            />
+            Blair
           </nuxt-link>
           <button
             type="button"
